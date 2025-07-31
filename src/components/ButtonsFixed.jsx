cde186d7fd63522b06259879333f9bd58c724546
luccasbarrosowaaz <luccas@waaz.com.br>
2025-07-31 15:21:57 -0300

    Update ButtonsFixed.jsx

--- src/components/ButtonsFixed.jsx
+++ src/components/ButtonsFixed.jsx
@@ -5,7 +5,7 @@ function ButtonsFixed() {
     <div className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col gap-3 z-50">
       <a
         id="whatsapp_button"
-        href="https://wa.me/5513996636009?text=Gostaria%20de%20informa%C3%A7%C3%B5es%20sobre%20bateria.%20Voc%C3%AAs%20podem%20me%20ajudar%3F"
+        href="https://api.whatsapp.com/send/?phone=5511968301193&text=Gostaria+de+informa%C3%A7%C3%B5es+sobre+bateria.+Voc%C3%AAs+podem+me+ajudar%3F&type=phone_number&app_absent=0"
         target="_blank"
         rel="noopener noreferrer"
         className="relative overflow-hidden flex items-center w-full gap-2 bg-[#b99921] rounded-full px-8 md:px-16 py-3 transition duration-300 transform hover:bg-[#8a6f19] hover:scale-105 text-white font-semibold"
@@ -21,7 +21,7 @@ function ButtonsFixed() {
 
       <a
         id="trackfone_button"
-        href="tel:+551333797500"
+        href="tel:+551146733100"
         className="relative overflow-hidden flex items-center w-full gap-2 bg-[#b99921] rounded-full px-8 md:px-16 py-3 transition duration-300 transform hover:bg-[#8a6f19] hover:scale-105 text-white font-semibold"
         style={{
           boxShadow:
